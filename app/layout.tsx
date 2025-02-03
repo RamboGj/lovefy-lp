bd6ace025fd1f0a1fd71550910a7c42571c31767
RamboGj <jpramboguanabara@hotmail.com>
2025-02-02 22:04:24 -0300

    feat: SEO and OG sample

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,29 @@ import './globals.css'
 import SmoothScrolling from '@/components/atoms/SmoothScrolling'
 
 export const metadata: Metadata = {
-  title: 'Lovefy',
-  description: 'Lovefy Landing Page',
+  title: 'Lovefy - Chat with AI Men, Trans & Women',
+  description:
+    'Lovefy lets you create an account and chat with personalized AI personas. Explore intimate conversations, ask questions, and engage in a private environment with AI-generated characters. Experience connection with the future of AI chat.',
+  alternates: {
+    canonical: 'https://lovefy-lp.vercel.app/',
+  },
+  icons: {
+    icon: '/favicon.icon',
+  },
+  openGraph: {
+    title: 'Lovefy - Chat with AI Men, Trans & Women',
+    description:
+      'Lovefy lets you create an account and chat with personalized AI personas. Explore intimate conversations, ask questions, and engage in a private environment with AI-generated characters. Experience connection with the future of AI chat.',
+    siteName: 'Lovefy',
+    url: 'https://lovefy-lp.vercel.app/',
+    type: 'website',
+    images: [
+      {
+        url: '/og.png',
+      },
+    ],
+  },
 }
-
 export default function RootLayout({
   children,
 }: Readonly<{
